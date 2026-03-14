3266ff4e04a3b65941517de00bc84bbe03b2ad68
Siddharth Pande <sidarjunstn.sp@gmail.com>
2026-03-14 15:15:51 +0530

    header designed responsiveness from md and above

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,12 +2,15 @@ import logoImage from "../../assets/images/Bigbasket-logo.svg"
 import { Search, Zap } from 'lucide-react';
 import { BiSolidZap } from "react-icons/bi";
 import { RiShoppingBasketLine } from "react-icons/ri";
+import { FaCaretDown } from "react-icons/fa";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+
 
 export default function Header() {
   return (
     <>
-      <header className='grid grid-cols-[1fr_minmax(0,1135px)_1fr] py-3 text-[13.5px]'>
-        <div className='header-outer lg:col-start-2'>
+      <header className='sticky top-0 grid grid-cols-[1fr_minmax(0,1135px)_1fr] py-3 text-[13.5px] shadow-xl border-t-4 border-green-700'>
+        <div className='header-outer hidden md:col-start-2 md:px-6 lg:px-10 xl:px-0 md:flex flex-col gap-6'>
           <div className='row-one flex'>
             <div className='logo w-23'>
               <img src={logoImage} alt="logo" />
@@ -18,10 +21,10 @@ export default function Header() {
                 <input type="text" placeholder="Search for products..." className="w-full focus:outline-none" />
               </label>
             </div>
-            <div className='location-badge cursor-pointer bg-gray-300 text-[10px] py-1 px-1 ml-auto rounded'>
-              <div className="text-green-600 flex items-center">
+            <div className='location-badge cursor-pointer bg-gray-200 text-[10px] py-1 px-1.5 ml-auto rounded'>
+              <div className="text-green-800 flex items-center">
                 <BiSolidZap className="" />
-                <span>Delivery in 10 mins</span>
+                <span className="font-bold">Delivery in 10 mins</span>
               </div>
               <span>Select Location</span>
             </div>
@@ -34,14 +37,66 @@ export default function Header() {
               </div>
             </div>
           </div>
-          <div className='row-two'>
-            <div className='category-btn'></div>
-            <div className='exotic'></div>
-            <div className='tea'></div>
-            <div className='ghee'></div>
-            <div className='Nandini'></div>
-            <div className='fresh-veg'></div>
-            <div className='smart-basket-badge'></div>
+          <div className='row-two flex items-center justify-between'>
+            <div className='category-btn flex bg-[#5D9400] rounded text-white items-center gap-2 py-2 px-3'>
+              <span className="font-bold">Shop by Category</span>
+              <FaCaretDown />
+            </div>
+            <div className='exotic'>
+              <span>Exotic Fruits & V...</span>
+            </div>
+            <div className='tea'>
+              <span>Tea</span>
+            </div>
+            <div className='ghee'>
+              <span>Ghee</span>
+            </div>
+            <div className='Nandini'>
+              <span>Nandini</span>
+            </div>
+            <div className='fresh-veg flex items-center gap-1'>
+              <span>Fresh Vegetables</span>
+              <MdKeyboardDoubleArrowRight />
+            </div>
+            <hr className="rotate-90 border-gray-300 w-5" />
+            <div className='smart-basket-badge mr-30 md:mr-0'>
+              <div>
+                <img src="https://www.bigbasket.com/media/uploads/banner_images/Smart_basket-250124-1.png" alt="badge" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='header-outer md:col-start-2 md:px-6 lg:px-10 xl:px-0 flex flex-col gap-6'>
+          <div className='row-one flex'>
+            <div className='logo w-23'>
+              <img src={logoImage} alt="logo" />
+            </div>
+
+            <div className='cart bg-pink-200 p-2 rounded ml-3 cursor-pointer'>
+              <div className="rounded-full bg-red-500 p-1">
+                <RiShoppingBasketLine size={17} className="text-white" />
+              </div>
+            </div>
+          </div>
+          <div className='drawer-content flex items-center justify-between'>
+            <div className='category-btn flex bg-[#5D9400] rounded text-white items-center gap-2 py-2 px-3'>
+              <span className="font-bold">Shop by Category</span>
+              <FaCaretDown />
+            </div>
+            <div className='search flex-1 mx-6'>
+              <label className="w-full relative border border-gray-400 focus-within:border-green-600 rounded py-2 px-8 flex items-center">
+                <Search className="absolute text-green-600 left-2" size={15} />
+                <input type="text" placeholder="Search for products..." className="w-full focus:outline-none" />
+              </label>
+            </div>
+            <div className='login-button text-[14px] text-white bg-black rounded py-2 px-4 ml-6 cursor-pointer'>
+              <span>Login/Sign up</span>
+            </div>
+            <div className='smart-basket-badge'>
+              <div>
+                <img src="https://www.bigbasket.com/media/uploads/banner_images/Smart_basket-250124-1.png" alt="badge" />
+              </div>
+            </div>
           </div>
         </div>
       </header >
